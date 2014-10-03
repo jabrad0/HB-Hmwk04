1a4d5363245c0d6771ca7e627ee4a76e9e51d2d4
JABbook <puff@Streamborns-MacBook-Pro.local>
2014-10-03 09:54:45 -0700

    Working code still need to improve though

--- accounting2.py
+++ accounting2.py
@@ -1,22 +1,17 @@
-def main():
-
-    customer_data=open("Hmwk04.csv")
+customer_data=open("customer_orders.csv")
     
-    melon_cost = 1.00
+melon_cost = 1.00
 
-    for line in customer_data:
-        data = line.split(",")
-        customer_name = data[1]
-        melons_purchased = int(data[2])
-        funds_paid = float(data[3])
+for line in customer_data:
+    data = line.split(",")
+    customer_name = data[1]
+    melons_purchased = int(data[2])
+    funds_paid = float(data[3])
+    customer_expected = melons_purchased * melon_cost
+    
+    if customer_expected != funds_paid:
+        print customer_name, "paid %.2f, expected %.2f"%(funds_paid, customer_expected)
 
-    for line in customer_data:
-        customer_expected = melons_purchased * melon_cost
-        if customer_expected != funds_paid:
-            print customer_name, "paid %.2f, expected %.2f"%(funds_paid, customer_expected)
 
+customer_data.close()
 
-    customer_data.close()
-
-if __name__ == "__main__":
-    main()
